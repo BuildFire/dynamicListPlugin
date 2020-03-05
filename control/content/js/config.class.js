f61e6e49bfa6051ebedb715a439029f0f192311c
mhd-rawashdah <mhd.rawashdah@gmail.com>
2020-03-06 00:57:38 +0200

    config data action class

--- control/content/js/config.class.js
+++ control/content/js/config.class.js
@@ -1,9 +1,34 @@
 class Config {
 
-  constructor(data) {
-    this.pluginData = data.pluginData;
-    this.querystring = data.querystring;
-    this.isPublic = data.isPublic;
+  constructor(dataObj) {
+    this.pluginData = dataObj.pluginData;
+    this.querystring = dataObj.querystring;
+    this.isPublic = dataObj.isPublic;
+  }
+
+  static get() {
+    return new Promise((resolve, reject) => {
+      buildfire.datastore.get("configs", (err, dataObj) => {
+        if (err) {
+          reject(err);
+        } else {
+          let config = new Config(dataObj);
+          resolve(config);
+        }
+      });
+    });
+  }
+
+  save() {
+    return new Promise((resolve, reject) => {
+      buildfire.datastore.save(this, "configs", (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
   }
 
 }
