fffc7aeb6688fc09147d4a1b4193e6ce7ae7f31e
mhd-rawashdah <mhd.rawashdah@gmail.com>
2020-03-07 14:50:47 +0200

    topic data action class

--- widget/js/topic.class.js
+++
@@ -1,9 +0,0 @@
-class Topic {
-
-  constructor(data) {
-    this.title = data.title;
-    this.type = data.type;
-    this.parentTopicId = data.parentTopicId || null;
-    this.reportedBy =  data.reportedBy || [];
-  }
-}
