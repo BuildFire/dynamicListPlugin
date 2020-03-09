e5e0ee61767df63a820727ab55cf585c0620d504
Mohammad Al Rawashdah <34889770+mhd-rawashdah@users.noreply.github.com>
2020-03-09 19:36:10 +0200

    Data Layer & Data Action Layer (#1)
    * config data class
    
    * topic data class
    
    * config data action class
    
    * topic data action class
    
    * create anyaltics class & register TOPIC_CRETED , TOPIC_REPORTED and TOPIC_DELETED events
    
    * (Analytics) track actions (TOPIC_CREATED, TOPIC_REPORTED, TOPIC_DELETD)
    
    * fix delete topic action
    
    * fix getTopics action

---
+++ control/content/js/classes/analytics.js
@@ -0,0 +1,32 @@
+class Analytics {
+
+ static events = {
+    TOPIC_CRETAED: 'TOPIC_CRETAED',
+    TOPIC_REPORTED: 'TOPIC_REPORTED',
+    TOPIC_DELETED: 'TOPIC_DELETED',
+  }
+
+  static init() {
+    this.registerEvent('Topic Created', this.events.TOPIC_CRETAED, 'Occurs when a user create a new topic', false);
+    this.registerEvent('Topic Reported', this.events.TOPIC_REPORTED, 'Occurs when a user report about a topic', false);
+    this.registerEvent('Topic Deleted', this.events.TOPIC_DELETED, 'Occurs when a user delete a topic', false);
+  }
+
+  static registerEvent(title, key, description, silentNotification) {
+    buildfire.analytics.registerEvent({
+      title,
+      key,
+      description
+    }, {
+      silentNotification
+    });
+  }
+
+  static trackAction(key, aggregationValue) {
+    let metData = {};
+    if (aggregationValue) {
+      metData._buildfire = {aggregationValue};
+    }
+    buildfire.analytics.trackAction(key, metData);
+  }
+}
